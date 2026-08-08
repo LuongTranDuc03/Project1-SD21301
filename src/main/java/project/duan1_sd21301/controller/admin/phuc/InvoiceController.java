5ffacd00c229337771f8207192fc62e39eb26e2e
Your Name <your.email@example.com>
2026-08-08 07:51:15 +0700

    Sửa luồng nhân viên

--- src/main/java/project/duan1_sd21301/controller/admin/phuc/InvoiceController.java
+++ src/main/java/project/duan1_sd21301/controller/admin/phuc/InvoiceController.java
@@ -36,7 +36,7 @@ import java.util.Map;
 })
 public class InvoiceController extends HttpServlet {
 
-    private static final int PAGE_SIZE = 5;
+    private static final int PAGE_SIZE = 10;
 
     private static final Map<Integer, String> ORDER_STATUS_LABELS_ONLINE;
     private static final Map<Integer, String> ORDER_STATUS_LABELS_POS;
